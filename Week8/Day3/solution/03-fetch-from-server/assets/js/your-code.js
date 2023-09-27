977b72023238757272d2a7550d5596682d74922d
gootieno <gotieno@geoffreys-MBP.attlocal.net>
2023-09-27 14:05:33 -0700

    fetch solutions

--- Week8/Day3/solution/03-fetch-from-server/assets/js/your-code.js
+++ Week8/Day3/solution/03-fetch-from-server/assets/js/your-code.js
@@ -1,25 +1,43 @@
 export function getAllDogs() {
   // Your code here
   const url = "/dogs";
-  
-  // Use the URLSearchParams API to format your body as shown below
 
+  // Use the URLSearchParams API to format your body as shown below
 
   return fetch(url);
 }
 
 export function getDogNumberTwo() {
   // Your code here
+  return fetch("/dogs/2");
 }
 
 export function postNewDog() {
   // Your code here
+  const options = {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams({ name: "Hachi Roku", age: "1" }),
+  };
+
+  return fetch("/dogs", options);
 }
 
 export function postNewDogV2(name, age) {
   // Your code here
+  const options = {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams({ name, age }), // {name:name, age:age}
+  };
+  return fetch("/dogs", options);
 }
 
 export function deleteDog(id) {
   // Your code here
+
+  return fetch(`/dogs/${id}/delete`, {
+    method: "POST",
+    headers: { AUTH: "ckyut5wau0000jyv5bsrud90y" },
+  });
 }
--- Week8/Day3/solution/04-fetch/fetch-requests.js
+++ Week8/Day3/solution/04-fetch/fetch-requests.js
@@ -7,20 +7,26 @@ Paste your code for fetch requests here once you finish each task.
 
 // Your code here
 
-
-
 /* ====== 2. Print true if the status of the response was successful ====== */
 
 // Your code here
-
-
 
 /* =================== 3. Print the Content-Type Header =================== */
 
 // Your code here
 
-
-
 /* ============== 4. Print the body of the response as text =============== */
 
 // Your code here
+const getBeautyProducts = async () => {
+  const response = await fetch("/categories/beauty/products");
+
+  console.log("response ", response);
+
+  console.log("response status ", response.status);
+  console.log("response ok? ", response.ok);
+
+  console.log('response text ', await response.text())
+};
+
+getBeautyProducts()
